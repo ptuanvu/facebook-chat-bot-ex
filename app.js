42729106931977b49ac5b5849d5f38a2d5cf6216
Vu Tran <monster@Monsters-MacBook-Air.local>
2017-12-08 14:55:04 +0700

    Third commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
  *
  * Starter Project for Messenger Platform Quick Start Tutorial
  *
- * Use this project as the starting point for following the 
+ * Use this project as the starting point for following the
  * Messenger Platform quick start tutorial.
  *
  * https://developers.facebook.com/docs/messenger-platform/getting-started/quick-start/
@@ -17,12 +17,13 @@
 'use strict';
 
 // Imports dependencies and set up http server
-const 
+const
   request = require('request'),
   express = require('express'),
   body_parser = require('body-parser'),
   app = express().use(body_parser.json()); // creates express http server
 
+const PAGE_ACCESS_TOKEN = "EAAZA7ZC1ZBadngBAEyMdasXSb2fwDFDqpDMwSGUsoaRU9ifYA0PCUV0MQUef4U42MJeVodKrjZAZB3CHJ0rXmfVYmLZCxZAxiUUs3mzTLTAuZCVSY8VZBYdX7zGjjlPGmy3I6mvFhegYZBatfTMZCB9OiatUYwQcu9LZBZAlZAy0agzmZCswJ2PktdZAsbpZC";
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 app.get('/',(req,res) => {
@@ -31,8 +32,60 @@ app.get('/',(req,res) => {
   }
   res.json(msg);
 });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }
+
+  // Sends the response message
+  callSendAPI(sender_psid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+
+}
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
+}
+
 // Accepts POST requests at /webhook endpoint
-app.post('/webhook', (req, res) => {  
+app.post('/webhook', (req, res) => {
 
   // Parse the request body from the POST
   let body = req.body;
@@ -43,11 +96,23 @@ app.post('/webhook', (req, res) => {
     // Iterate over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-      // Get the webhook event. entry.messaging is an array, but 
+      // Get the webhook event. entry.messaging is an array, but
       // will only ever contain one event, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
-      
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
+
     });
 
     // Return a '200 OK' response to all events
@@ -62,28 +127,26 @@ app.post('/webhook', (req, res) => {
 
 // Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
-  
+
   /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = "test_token";
-  
-  // Parse params from the webhook verification request
+  const VERIFY_TOKEN = "test_token";  // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
-    
+
   // Check if a token and mode were sent
   if (mode && token) {
-  
+
     // Check the mode and token sent are correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
+
       // Respond with 200 OK and challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
-    
+
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
+      res.sendStatus(403);
     }
   }
 });
