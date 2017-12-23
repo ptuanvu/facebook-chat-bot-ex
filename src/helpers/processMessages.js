0ba601106130759ea22804419a3916561c8487c0
zinbac000 <zinbac000@gmail.com>
2017-12-23 10:43:52 +0700

    .

--- src/helpers/processMessages.js
+++ src/helpers/processMessages.js
@@ -3,24 +3,6 @@ import { callSendAPI, callApiSendMessage } from './sendAPI';
 import { Players } from '../lib';
 
 const request = require('request');
-const apiAiClient = require('apiai')(API_AI_TOKEN);
-
-
-const requestToAI_API = (senderId, message) => {
-  const apiaiSession = apiAiClient.textRequest(message, {sessionId: 'facebook-chat-bot-ex'});
-  apiaiSession.on('response', (response) => {
-      console.log(response);
-      var aiText = response.result.fulfillment.speech;
-      if (response.result.metadata.intentName === 'players') {
-          callApiSendMessage(senderId, aiText);
-      } else {
-          sendTheMenu(senderId);
-      }
-  });
-
-  apiaiSession.on('error', error => console.log(error));
-  apiaiSession.end();
-}
 
 export function handleMessages(event) {
   const senderId = event.sender.id;
